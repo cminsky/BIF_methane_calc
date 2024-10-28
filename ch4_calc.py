e71fd323fe1af6f080bdad54eab206108a251924
Charlotte Minsky <cminsky@users.noreply.github.com>
2024-10-28 12:56:58 -0400

    updates

--- ch4_calc.py
+++ ch4_calc.py
@@ -1,13 +1,34 @@
 import streamlit as st
 import numpy as np
 
+state = st.radio(label='Set parameters',
+                 options=['Min','Mid','Max'],
+                 index = 1,
+                 horizontal=True)
+
+if state == 'Min':
+    d_BIF_mass = 7.3
+    d_Fe_wt = 0.2
+    d_atmos_pressure = 1.1
+elif state == 'Max':
+    d_BIF_mass = 10.
+    d_Fe_wt = 0.4
+    d_atmos_pressure = 0.23
+else:
+    d_BIF_mass = 8.7
+    d_Fe_wt = 0.3
+    d_atmos_pressure = 0.5
+    
+
+"---"
+
 col1,col2,col3 = st.columns(3)
 with col1:
     BIF_mass = st.slider(label='Global BIF mass (1e20 g)',
-                        min_value=7.3,max_value=10.,
+                        min_value=7.3,max_value=10.,value=d_BIF_mass,
                         help='Minimum from sum of Bekker et al. 2010')
     Fe_wt = st.slider(label='Average BIF Fe content (wt %)',
-                      min_value=0.2,max_value=0.4,
+                      min_value=0.2,max_value=0.4,value=d_Fe_wt,
                       help='Range from Klein et al. 2005')
     
     Fe_mass = Fe_wt*BIF_mass*1e20 # mass of Fe (g)
@@ -17,10 +38,10 @@ with col1:
     "BIF Fe: %0.2e g"%Fe_mass,"$\div$ 56 g/mol =","%0.2e mol"%Fe_moles
 
 with col2:
-    Fe_C_ratio = st.number_input(label='Stoichimoetric Fe\:C',
+    Fe_C_ratio = st.number_input(label='Stoichiometric Fe\:C ratio',
                                  value=12,
                                  help='Default from Thibon et al. 2019 eq. 13')
-    CH4_percent = st.slider(label='Portion of BIF formed via CH4 production',
+    CH4_percent = st.slider(label='Fraction of BIF formed with CH4 production',
                         min_value=0.,max_value=1.,value=1.)
 
     Fe_moles_CH4 = Fe_moles*CH4_percent
@@ -31,18 +52,18 @@ with col2:
 
 with col3:
     atmos_pressure = st.slider(label='Atmospheric pressure relative to modern',
-                            min_value=0.,max_value=1.1,value=0.5,
+                            min_value=0.,max_value=1.1,value=d_atmos_pressure,
                             help='Range from Catling et al. 2020')
     NA = 1.87e20 # moles air in the atmosphere
         # Charlotte says just trust me bro (1 atm times Earth surface area)
     
     # Archean atmosphere air moles
     air_moles = NA*atmos_pressure
-    "Atmosphere: %0.2e mol"%air_moles
+    "Archean atmos: %0.2e mol"%air_moles
 
 "---"
 "**Result:**"
 CH4_ppm = CH4_moles/air_moles*1e6
-"[CH4] = %0.2f ppm"%CH4_ppm
+f"[CH4] = {CH4_ppm:,.0f} ppm"
 
 st.caption("Compare to baseline Archean [CH4] estimate: 100-10,000 ppm (Catling et al. 2020)")
